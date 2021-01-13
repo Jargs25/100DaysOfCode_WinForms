27fc137c42e580e3f9cabe32ac74906cc454d922
Jared <jazared_@hotmail.com>
2021-01-13 06:20:04 -0600

    Primer commit

---
+++ 100DaysOdCode_WinForms/frmProductos.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace _100DaysOdCode_WinForms
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gbxDatos = new System.Windows.Forms.GroupBox();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnSubirImagen = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.gbxRegistros = new System.Windows.Forms.GroupBox();
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ofdSubirImagen = new System.Windows.Forms.OpenFileDialog();
+            this.lblNoDisponible = new System.Windows.Forms.Label();
+            this.gbxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.gbxRegistros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxDatos
+            // 
+            this.gbxDatos.Controls.Add(this.lblNoDisponible);
+            this.gbxDatos.Controls.Add(this.pbxImagen);
+            this.gbxDatos.Controls.Add(this.btnBuscar);
+            this.gbxDatos.Controls.Add(this.btnEliminar);
+            this.gbxDatos.Controls.Add(this.btnModificar);
+            this.gbxDatos.Controls.Add(this.btnSubirImagen);
+            this.gbxDatos.Controls.Add(this.btnAgregar);
+            this.gbxDatos.Controls.Add(this.txtPrecio);
+            this.gbxDatos.Controls.Add(this.lblPrecio);
+            this.gbxDatos.Controls.Add(this.txtCantidad);
+            this.gbxDatos.Controls.Add(this.lblCantidad);
+            this.gbxDatos.Controls.Add(this.txtNombre);
+            this.gbxDatos.Controls.Add(this.lblNombre);
+            this.gbxDatos.Controls.Add(this.txtCodigo);
+            this.gbxDatos.Controls.Add(this.lblCodigo);
+            this.gbxDatos.Location = new System.Drawing.Point(12, 12);
+            this.gbxDatos.Name = "gbxDatos";
+            this.gbxDatos.Size = new System.Drawing.Size(331, 191);
+            this.gbxDatos.TabIndex = 0;
+            this.gbxDatos.TabStop = false;
+            this.gbxDatos.Text = "Datos";
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.Location = new System.Drawing.Point(15, 19);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(127, 127);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 10;
+            this.pbxImagen.TabStop = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(241, 152);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(160, 152);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Enabled = false;
+            this.btnModificar.Location = new System.Drawing.Point(241, 123);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnSubirImagen
+            // 
+            this.btnSubirImagen.Location = new System.Drawing.Point(15, 152);
+            this.btnSubirImagen.Name = "btnSubirImagen";
+            this.btnSubirImagen.Size = new System.Drawing.Size(127, 23);
+            this.btnSubirImagen.TabIndex = 6;
+            this.btnSubirImagen.Text = "Agregar Imagen";
+            this.btnSubirImagen.UseVisualStyleBackColor = true;
+            this.btnSubirImagen.Click += new System.EventHandler(this.btnSubirImagen_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(160, 123);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(212, 97);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(104, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(157, 100);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio.TabIndex = 6;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(212, 71);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(104, 20);
+            this.txtCantidad.TabIndex = 3;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(157, 74);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(212, 45);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(104, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(157, 48);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(212, 19);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(104, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(157, 22);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // gbxRegistros
+            // 
+            this.gbxRegistros.Controls.Add(this.dgvRegistros);
+            this.gbxRegistros.Location = new System.Drawing.Point(353, 12);
+            this.gbxRegistros.Name = "gbxRegistros";
+            this.gbxRegistros.Size = new System.Drawing.Size(299, 191);
+            this.gbxRegistros.TabIndex = 1;
+            this.gbxRegistros.TabStop = false;
+            this.gbxRegistros.Text = "Registros";
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.AllowUserToAddRows = false;
+            this.dgvRegistros.AllowUserToDeleteRows = false;
+            this.dgvRegistros.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegistros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvRegistros.Location = new System.Drawing.Point(15, 22);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegistros.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvRegistros.RowHeadersVisible = false;
+            this.dgvRegistros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRegistros.Size = new System.Drawing.Size(267, 153);
+            this.dgvRegistros.TabIndex = 0;
+            this.dgvRegistros.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvRegistros_CellMouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Precio";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "RutaImagen";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // ofdSubirImagen
+            // 
+            this.ofdSubirImagen.FileName = "NoDisponible";
+            // 
+            // lblNoDisponible
+            // 
+            this.lblNoDisponible.AutoSize = true;
+            this.lblNoDisponible.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoDisponible.Location = new System.Drawing.Point(34, 64);
+            this.lblNoDisponible.Name = "lblNoDisponible";
+            this.lblNoDisponible.Size = new System.Drawing.Size(91, 38);
+            this.lblNoDisponible.TabIndex = 11;
+            this.lblNoDisponible.Text = "No\r\nDisponible";
+            this.lblNoDisponible.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblNoDisponible.Visible = false;
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 222);
+            this.Controls.Add(this.gbxRegistros);
+            this.Controls.Add(this.gbxDatos);
+            this.Name = "frmProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD Productos";
+            this.gbxDatos.ResumeLayout(false);
+            this.gbxDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.gbxRegistros.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxDatos;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.GroupBox gbxRegistros;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvRegistros;
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.Button btnSubirImagen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.OpenFileDialog ofdSubirImagen;
+        private System.Windows.Forms.Label lblNoDisponible;
+    }
+}
+
---
+++ 100DaysOdCode_WinForms/frmProductos.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _100DaysOdCode_WinForms
+{
+    public partial class frmProductos : Form
+    {
+        string rutaImagen = Directory.GetCurrentDirectory();
+
+        public frmProductos()
+        {
+            InitializeComponent();
+
+            rutaImagen = rutaImagen.Replace("bin\\Debug","Productos");
+            if (!Directory.Exists(rutaImagen)) Directory.CreateDirectory(rutaImagen);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (sonValidos())
+            {
+                string nombreImagen = ofdSubirImagen.SafeFileName;
+                string[] datos = new string[]
+                {
+                txtCodigo.Text.Trim(),
+                txtNombre.Text.Trim(),
+                txtCantidad.Text.Trim(),
+                txtPrecio.Text.Trim(),
+                nombreImagen == "NoDisponible" ? nombreImagen : rutaImagen + "\\" + nombreImagen
+                };
+                dgvRegistros.Rows.Add(datos);
+
+                if (nombreImagen != "NoDisponible")
+                    pbxImagen.Image.Save(rutaImagen + "\\" + nombreImagen);
+                limpiarForm();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+            }
+        }
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (sonValidos())
+            {
+                string nombreImagen = ofdSubirImagen.SafeFileName;
+
+            dgvRegistros.Rows[id].Cells[0].Value = txtCodigo.Text.Trim();
+            dgvRegistros.Rows[id].Cells[1].Value = txtNombre.Text.Trim();
+            dgvRegistros.Rows[id].Cells[2].Value = txtCantidad.Text;
+            dgvRegistros.Rows[id].Cells[3].Value = txtPrecio.Text;
+            dgvRegistros.Rows[id].Cells[4].Value = nombreImagen == "NoDisponible" ? nombreImagen : rutaImagen + "\\" + nombreImagen;
+
+            if (nombreImagen != "NoDisponible")
+                pbxImagen.Image.Save(rutaImagen + "\\" + nombreImagen);
+            limpiarForm();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+            }
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            dgvRegistros.Rows.RemoveAt(id);
+            string imagen = ofdSubirImagen.FileName;
+            limpiarForm();
+            File.Delete(imagen);
+        }
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (btnBuscar.Text == "Buscar")
+            {
+                for (int i = 0; i < dgvRegistros.Rows.Count; i++)
+                {
+                    if (dgvRegistros.Rows[i].Cells[0].Value.ToString().Contains(txtCodigo.Text.Trim()) &&
+                        dgvRegistros.Rows[i].Cells[1].Value.ToString().Contains(txtNombre.Text.Trim()) &&
+                        dgvRegistros.Rows[i].Cells[2].Value.ToString().Contains(txtCantidad.Text.Trim()) &&
+                        dgvRegistros.Rows[i].Cells[3].Value.ToString().Contains(txtPrecio.Text.Trim()))
+                    {
+                        dgvRegistros.Rows[i].Visible = true;
+                    }
+                    else
+                    {
+                        dgvRegistros.Rows[i].Visible = false;
+                    }
+                }
+            }
+            else
+            {
+                limpiarForm();
+                btnBuscar.Text = "Buscar";
+            }
+            
+        }
+
+        private void btnSubirImagen_Click(object sender, EventArgs e)
+        {
+            ofdSubirImagen.ShowDialog();
+            if (ofdSubirImagen.FileName != "NoDisponible")
+            {
+                lblNoDisponible.Visible = false;
+                pbxImagen.Image = Image.FromFile(ofdSubirImagen.FileName);
+            }
+            else
+            {
+                lblNoDisponible.Visible = true;
+            }
+        }
+        int id = 0;
+
+        private void dgvRegistros_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = e.RowIndex;
+
+            if(id > -1)
+            {
+                txtCodigo.Text = dgvRegistros.Rows[id].Cells[0].Value.ToString();
+                txtNombre.Text = dgvRegistros.Rows[id].Cells[1].Value.ToString();
+                txtCantidad.Text = dgvRegistros.Rows[id].Cells[2].Value.ToString();
+                txtPrecio.Text = dgvRegistros.Rows[id].Cells[3].Value.ToString();
+                ofdSubirImagen.FileName = dgvRegistros.Rows[id].Cells[4].Value.ToString();
+
+                if (ofdSubirImagen.FileName != "NoDisponible")
+                {
+                    lblNoDisponible.Visible = false;
+                    pbxImagen.Image = Image.FromFile(ofdSubirImagen.FileName);
+                }
+                else
+                {
+                    lblNoDisponible.Visible = true;
+                }
+
+                btnModificar.Enabled = true;
+                btnEliminar.Enabled = true;
+            }
+            btnBuscar.Text = "Limpiar";
+            btnAgregar.Enabled = false;
+        }
+
+        public bool sonValidos()
+        {
+            return txtCodigo.Text.Trim() != "" &&
+                txtNombre.Text.Trim() != "" &&
+                txtCantidad.Text.Trim() != "" &&
+                txtPrecio.Text.Trim() != "";
+        }
+        public void limpiarForm()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtCantidad.Clear();
+            txtPrecio.Clear();
+            lblNoDisponible.Visible = false;
+            btnAgregar.Enabled = true;
+            btnModificar.Enabled = false;
+            btnEliminar.Enabled = false;
+            ofdSubirImagen.FileName = "NoDisponible";
+            if (pbxImagen.Image != null)
+                pbxImagen.Image.Dispose();
+            pbxImagen.Image = null;
+            id = 0;
+        }
+
+    }
+}
